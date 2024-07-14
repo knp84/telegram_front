ec884300cbaa9c0244eec538940f0342ea9782af
knp84 <sabanovroman84@gmail.com>
2024-07-14 18:47:40 +0500

    Delete keyboard_pr.py

--- app/keyboard_pr.py
+++
@@ -1,9 +0,0 @@
-from aiogram import types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-main = ReplyKeyboardMarkup(keyboard=[[types.KeyboardButton(text='Уровень 1')],
-                                    [types.KeyboardButton(text='Уровень 2')],
-                                    [types.KeyboardButton(text='Уровень 3 WIP'),
-                                    types.KeyboardButton(text='уровень 4 WIP')]],
-                        resize_keyboard=True,
-                        input_field_placeholder='Выберите уровень сложности....')

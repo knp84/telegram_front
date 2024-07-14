868cc2c045726c4931c69ce72d69e440c3e830f1
knp84 <sabanovroman84@gmail.com>
2024-07-15 01:08:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 from app.handlers import router
 from config import API_TOKEN
 async def main():
-   bot = Bot(token=)
+   bot = Bot(token=API_TOKEN)
    dp = Dispatcher()
    dp.include_router(router)
    await dp.start_polling(bot)

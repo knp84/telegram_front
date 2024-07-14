19f7c89607f6e758877ab96c3d31ef2438429913
knp84 <sabanovroman84@gmail.com>
2024-07-14 18:57:40 +0500

    Update core.py

--- core.py
+++ core.py
@@ -4,19 +4,18 @@ from random import randint
 a = randint(0,100)
 b = randint(0,100)
 
-determine = 1
-addition_plus = a + b
-addition_player = str(addition_plus)
+plus = a + b
 plus = (f'Введите сумму чисел {a} и {b}: ')
-pon_1 = (f'да, {a} + {b} = {addition_plus}')         
 
-pon = ('Неверный ответ!')  
-print(addition_player)
+correct_result_plus = (f'да, {a} + {b} = {plus}')         
+wrong_result = ('Неверный ответ!')  
+
+print(plus)
 
 
 c = randint(0,10)
 d = randint(0,10)
-multyplie = d * c
-multyplie_player = str(multyplie)
+
+multiply = d * c
 plus_1 = (f'Введите произведение чисел {d} и {c}: ')
-pon_2 = (f'да, {d} * {c} = {multyplie}')   
+correct_result_multiply = (f'да, {d} * {c} = {multiply}')   

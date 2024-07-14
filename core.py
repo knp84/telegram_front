db0af604019fd2d0597b1b00c05310aa39e10426
knp84 <sabanovroman84@gmail.com>
2024-07-14 18:53:25 +0500

    Update core.py

--- core.py
+++ core.py
@@ -1,30 +1,9 @@
 from math import sqrt
 from random import randint
-def player_solution():
-    match determine:                                 #определяет какой тип нужно проверять
-        case 1:
-            try:
-                solution = int(input())                                    
-            except:                                                            
-                return print('Ошибка ввода! Введите число!')                
-        case 2:
-            try:
-                solution = float(input('округлите x до первого знака после запятой '))
-            except:
-                print('Введите число с плавающей точкой!')
-        case 3:
-            solution = input()
-            try:
-                int(solution)
-            except:
-                print('Введите строковое значение')
-    return solution
-
-                               
+                           
 a = randint(0,100)
 b = randint(0,100)
-c = randint(0,10)
-d = randint(0,10)
+
 determine = 1
 addition_plus = a + b
 addition_player = str(addition_plus)
@@ -34,6 +13,9 @@ pon_1 = (f'да, {a} + {b} = {addition_plus}')
 pon = ('Неверный ответ!')  
 print(addition_player)
 
+
+c = randint(0,10)
+d = randint(0,10)
 multyplie = d * c
 multyplie_player = str(multyplie)
 plus_1 = (f'Введите произведение чисел {d} и {c}: ')

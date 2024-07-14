10226cf1ac1805961775f4b60350d251c17bc7b7
knp84 <sabanovroman84@gmail.com>
2024-07-14 19:00:45 +0500

    Update core.py

--- core.py
+++ core.py
@@ -5,10 +5,10 @@ a = randint(0,100)
 b = randint(0,100)
 
 plus = a + b
-plus = (f'Введите сумму чисел {a} и {b}: ')
+question_plus = (f'Введите сумму чисел {a} и {b}: ')
 
 correct_result_plus = (f'да, {a} + {b} = {plus}')         
-wrong_result = ('Неверный ответ!')  
+
 
 print(plus)
 
@@ -17,5 +17,8 @@ c = randint(0,10)
 d = randint(0,10)
 
 multiply = d * c
-plus_1 = (f'Введите произведение чисел {d} и {c}: ')
+question_multiply = (f'Введите произведение чисел {d} и {c}: ')
 correct_result_multiply = (f'да, {d} * {c} = {multiply}')   
+
+
+wrong_result = ('Неверный ответ!')  

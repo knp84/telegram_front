6753d52558187f397fb6c8c9e8f45f076e76ffd2
knp84 <sabanovroman84@gmail.com>
2024-07-15 11:38:34 +0500

    Update core.py

--- core.py
+++ core.py
@@ -1,12 +1,12 @@
 from math import sqrt
 from random import randint
-from app.handlers import loop_f
 spisok= list(range(0,99999)) #ничего лучше в голову не лезет
 spisok_str = str(spisok)
          
 #позже попытаюсь сделать базу данных(если мне это чем то поможет), а пока что просто не могу ввести разнообразие примеров
+i = 5
 
-for i in range(1, loop_f):
+for i in range(1, i):
     a = randint(0,100)
     b = randint(0,100)
         
@@ -17,16 +17,17 @@ for i in range(1, loop_f):
 
     print(plus)
 
-      
-    
+    print(i)
+        
+        
 
 
-c = randint(0,10)
-d = randint(0,10)
+    c = randint(0,10)
+    d = randint(0,10)
 
-multiply = str(d * c)
-question_multiply = (f'Введите произведение чисел {d} и {c}: ')
-correct_result_multiply = (f'да, {d} * {c} = {multiply}')   
+    multiply = str(d * c)
+    question_multiply = (f'Введите произведение чисел {d} и {c}: ')
+    correct_result_multiply = (f'да, {d} * {c} = {multiply}')   
 
 
-wrong_result = ('Неверный ответ!')  
+    wrong_result = ('Неверный ответ!')  

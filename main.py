4f058c7ea5b4dcd504bf611ca90bb961a8b8ab67
knp84 <sabanovroman84@gmail.com>
2024-07-15 10:18:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import  asyncio
 from aiogram import Bot, Dispatcher
 from app.handlers import router
 from config import API_TOKEN
+
 async def main():
    bot = Bot(token=API_TOKEN)
    dp = Dispatcher()

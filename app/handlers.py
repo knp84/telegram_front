91136dac18aa7a9f4c13ca2c0d33613179765f34
knp84 <sabanovroman84@gmail.com>
2024-07-15 12:03:53 +0500

    Update handlers.py

--- app/handlers.py
+++ app/handlers.py
@@ -29,7 +29,7 @@ async def send_welcome(message: Message):
 async def Stop(message: Message,state: FSMContext):
    await message.answer('ok')
    await state.set_state(Stopping.stop)
-   core.i += 1
+   core.plus_func()
 
 @router.message(F.text=='Выбрать уровень сложности')
 async def levels_dif(message: Message):
@@ -85,7 +85,7 @@ async def level_2(message: Message, state: FSMContext):
 @router.message(Stopping.resume)
 async def Resume(message: Message, state: FSMContext):
    
-   core.i += 1
+   core.plus_func()
 
    if message.text == 'нет':
       await state.set_state(Stopping.stop)

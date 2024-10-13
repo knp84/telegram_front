f2534197bb92979419d4f37392171aef8a805c7e
knp84 <sabanovroman84@gmail.com>
2024-10-13 13:05:56 +0500

    Update middlewares.py

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,14 +1,14 @@
 from typing import Any, Awaitable, Callable, Dict
 from aiogram import BaseMiddleware
-from aiogram.types import Message
-from handlers import level_1
+from aiogram.types import Message, CallbackQuery
+from app.handlers import level_1, CallbackQuery
 
 class TestMiddleware(BaseMiddleware):
     async def __call__(
         self,
-        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
-        data: Dict[str, level_1]
+        handler: Callable[[CallbackQuery, Dict[CallbackQuery, level_1]], Awaitable[Any]],
+        event: CallbackQuery,
+        data: Dict[CallbackQuery, level_1]
     ) -> Any:
         print('123')
         return await handler(event, data)

9a9243acb0f001eea76319aebbcf08f771e93988
knp84 <sabanovroman84@gmail.com>
2024-10-14 07:51:11 +0500

    Update middlewares.py

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,15 +1,22 @@
 from typing import Any, Awaitable, Callable, Dict
 from aiogram import BaseMiddleware
-from aiogram.types import Message, CallbackQuery
-from app.handlers import level_1, CallbackQuery
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery
+
 
 class TestMiddleware(BaseMiddleware):
     async def __call__(
         self,
-        handler: Callable[[Message, Dict[str, Any]], Awaitable[Any]],
-        event: Message,
+        handler: Callable[[CallbackQuery, Dict[str, Any]], Awaitable[Any]],
+        event: CallbackQuery,
         data: Dict[str, Any]
     ) -> Any:
+        
+        print('все ок')
+
         return await handler(event, data)
+        
+        
+        
+
+    
+    

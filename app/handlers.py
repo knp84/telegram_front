c92487dc6b131bdcd3df156b976f1edebbc46937
knp84 <sabanovroman84@gmail.com>
2025-04-26 16:42:46 +0500

    first work ver.

--- app/handlers.py
+++ app/handlers.py
@@ -2,13 +2,12 @@ from aiogram import  Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import CommandStart, Command
 
-
 from app.keyboard import main, Levels
 
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 
-import core
+from random import randint
 
 router = Router()
 
@@ -18,6 +17,14 @@ class CallbackQuery(StatesGroup):
    level_3 = State()
    level_4 = State()
 
+def player_solution(mark):
+   a = randint(0,100)
+   b = randint(0,100)
+   if mark == 1:
+      plus = str(a + b)
+      question_plus = (f'Введите сумму чисел {a} и {b}: ')
+      correct_result_plus = (f'да, {a} + {b} = {plus}')      
+      return plus, question_plus, correct_result_plus
 class Stopping(StatesGroup):
    stop = State()
    resume = State()
@@ -40,43 +47,45 @@ async def levels_dif(message: Message):
    
 @router.callback_query(F.data == 'level_one')               #даю сатусы
 async def level_one(callback: CallbackQuery, state: FSMContext):    
-   
+   global plus, correct_result_plus
+   plus, question_plus, correct_result_plus = player_solution(1)
    await callback.answer('Уровень 1: Cложение')
-   await callback.message.answer(core.question_plus)
+   await callback.message.answer(question_plus)
    await state.set_state(CallbackQuery.level_1)
    
    
       
-
+'''
    
 @router.callback_query(F.data == 'level_two')
 async def level_two(callback: CallbackQuery, state: FSMContext):      
    await callback.answer('Уровень 2: Умножение')
    await callback.message.answer(core.question_multiply) 
    await state.set_state(CallbackQuery.level_2)
+'''
 
 @router.message(CallbackQuery.level_1)                #реагирую на статусы
 async def level_1(message: Message, state: FSMContext):
    await state.update_data(level_1=message.text)
 
-   if message.text in core.spisok_str:
-      if message.text == core.plus:
-         await message.answer(core.correct_result_plus)
+   if message.text in [str(i) for i in range(0,99999)]:
+      if message.text == plus:
+         await message.answer(correct_result_plus)
       else:
-         await message.answer(core.wrong_result)
+         await message.answer('Неверный ответ!')
    else:
       await message.answer('Введите число!')
       
    await state.set_state(Stopping.resume)
    await message.answer('Продолжить? да/нет')
    
-
+'''
    
 @router.message(CallbackQuery.level_2)
 async def level_2(message: Message, state: FSMContext):
    await state.update_data(level_2=message.text)
    
-   if message.text in core.spisok_str:
+   if message.text in list(range(0,99999)):
       if message.text == core.multiply:
          await message.answer(core.correct_result_multiply)
       else:
@@ -86,7 +95,7 @@ async def level_2(message: Message, state: FSMContext):
    
    await state.set_state(Stopping.resume)
    await message.answer('Продолжить? да/нет')
-
+'''
 #level_3 and level_4 later
 
 @router.message(Stopping.resume)

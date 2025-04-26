32c33949895e365b615fbb00b5773ee1ddba87b6
knp84 <sabanovroman84@gmail.com>
2025-04-26 17:35:23 +0500

    venv

--- app/middlewares.py
+++ app/middlewares.py
@@ -1,6 +1,9 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from typing import Any, Awaitable, Callable, Dict
 from aiogram import BaseMiddleware
-from aiogram.types import CallbackQuery
+from aiogram.types import CallbackQuery, TelegramObject 
 
 
 class TestMiddleware(BaseMiddleware):
@@ -15,7 +18,20 @@ class TestMiddleware(BaseMiddleware):
     
         return await handler(event, data)
     
-    
+class DatabaseMiddleware(BaseMiddleware):
+    def __init__(self, session: async_sessionmaker[AsyncSession]) -> None:
+        self.session = session
+
+    async def __call__(
+        self, 
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject, 
+        data: Dict[str, Any]) -> Any:
+
+        async with self.session() as session:
+            db = Database(session=session)
+            data['db'] = db
+            return await handler(event, data)
 
 
 

1bdbd76de17c79bbbd5681fd11b5e70bbc493594
knp84 <sabanovroman84@gmail.com>
2025-04-30 20:49:06 +0500

    bd

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import  asyncio 
 from aiogram import Bot, Dispatcher
-from app.handlers import router
-from app.middlewares import TestMiddleware
-from config import API_TOKEN
 
-router.callback_query.middleware(TestMiddleware())
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from app.handlers import router
+from app.middlewares import DatabaseMiddleware
+from config import API_TOKEN, load_config, Config
+
+
 
 
 async def main():
-   bot = Bot(token=API_TOKEN)
+   config: Config = load_config()
+   engine = create_async_engine(url=config.db.url, echo=True)
+   session = async_sessionmaker(engine, expire_on_commit=False)
+   
+   bot = Bot(token=API_TOKEN, parse_mode='HTML')
    dp = Dispatcher()
    dp.include_router(router)
+   dp.update.middleware(DatabaseMiddleware(session=session))
+   await bot.delete_webhook(drop_pending_updates=True)
    await dp.start_polling(bot)
    
 
